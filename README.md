6254b4247176e773f51a4ae56a7f82364e67b80c
채소 <chlthgus0901@naver.com>
2024-07-05 23:44:11 +0900

    feat: 팝업 기능 구현
    - main.js 생성
    - 팝업 닫기, 오늘 하루 안 보기 기능 구현

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eight-lane-highway
+
 멋쟁이사자처럼 테킷 프론트엔드 스쿨 10기 8조 바닐라 프로젝트 저장소
+
+2024-07-05 23:40 최소현 chlthgus0901@naver.com
+
+src/component/pop-up/main.js : main.js 생성 및 팝업 닫기, 오늘 하루 안 보기 기능 구현
--- src/component/pop-up/main.js
+++ src/component/pop-up/main.js
@@ -1,12 +1,42 @@
-// 창 닫기
-function closePopup() {}
+const popup = document.querySelector(".pop-up-wrapper");
+const hideButton = document.querySelector("#hidePopup");
+const closeButton = document.querySelector("#closePopup");
 
-// 팝업창 띄우기(페이지 로드시)
-function showPopup() {}
+// 팝업창 닫기
+function closePopup() {
+  popup.style.display = "none";
+}
 
-// 쿠키 설정
-const handleCookie = {
-  setCookie(key, value, exp) {},
+//storage 값을 가져왔을때 현재시간보다 크면 visibilty hidden
 
-  getCookie(key) {},
-};
+async function compare() {
+  let now = new Date();
+  now = now.setTime(now.getTime());
+  let hideToday = (await localStorage.getItem("popupExp")) > now;
+  if (hideToday) {
+    popup.style.visibility = "hidden";
+  }
+}
+
+//페이지 로딩후 팝업 로딩 여부
+document.addEventListener("DOMContentLoaded", compare);
+
+// 팝업 하루동안 안보이기
+function hidePopup() {
+  popup.style.display = "none";
+  setStorage("popupExp", 1);
+}
+
+// 오늘 하루 안 보기 만료 시간 storage 저장
+
+function setStorage(name, exp) {
+  // 만료 시간 구하기(exp를 ms단위로 변경)
+  let date = new Date();
+  date = date.setTime(date.getTime() + exp * 60 * 1000);
+
+  // 로컬 스토리지에 저장하기
+  localStorage.setItem(name, date);
+}
+
+hideButton.addEventListener("click", hidePopup);
+closeButton.addEventListener("click", closePopup);
