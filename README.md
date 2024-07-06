efee42d8f55eccc005967a94a1e8ba13d3b47bfe
채소 <chlthgus0901@naver.com>
2024-07-06 16:27:38 +0900

    feat: popup 기능 수정 및 스타일링
    - `div`에서 `dialog`로 기능 수정
    - `div`에서 `dialog`로 마크업 수정
    - popup 기본 스타일링

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 멋쟁이사자처럼 테킷 프론트엔드 스쿨 10기 8조 바닐라 프로젝트 저장소
 
-2024-07-05 23:40 최소현 chlthgus0901@naver.com
+2024-07-05 13:30 최소현
 
-src/component/pop-up/main.js : main.js 생성 및 팝업 닫기, 오늘 하루 안 보기 기능 구현
+src/component/pop-up/index.html : `index.html` 생성 및 마크업
+
+2024-07-05 23:40 최소현
+
+src/component/pop-up/main.js : `main.js` 생성 및 팝업 닫기, 오늘 하루 안 보기 기능 구현
+
+2024-07-06 16:21 최소현
+src/component/pop-up/main.js : `div`에서 `dialog`로 기능 수정
+src/component/pop-up/index.html : `div`에서 `dialog`로 마크업 수정
+src/component/pop-up/style.scss : popup 기본 스타일링
--- src/component/pop-up/main.js
+++ src/component/pop-up/main.js
@@ -1,20 +1,26 @@
-const popup = document.querySelector(".pop-up-wrapper");
+let popup = document.querySelector(".pop-up-container");
 const hideButton = document.querySelector("#hidePopup");
 const closeButton = document.querySelector("#closePopup");
+const buttonGroup = document.querySelector(".close-hide-btn");
+
+popup.showModal();
 
 // 팝업창 닫기
 function closePopup() {
+  popup.close();
   popup.style.display = "none";
 }
 
 //storage 값을 가져왔을때 현재시간보다 크면 visibilty hidden
+// style 인라인 속성 => 클래스로 스타일값 주기
 
 async function compare() {
   let now = new Date();
   now = now.setTime(now.getTime());
   let hideToday = (await localStorage.getItem("popupExp")) > now;
   if (hideToday) {
-    popup.style.visibility = "hidden";
+    popup.close();
+    popup.style.display = "none";
   }
 }
 
@@ -23,6 +29,7 @@ document.addEventListener("DOMContentLoaded", compare);
 
 // 팝업 하루동안 안보이기
 function hidePopup() {
+  popup.close();
   popup.style.display = "none";
   setStorage("popupExp", 1);
 }
@@ -30,13 +37,26 @@ function hidePopup() {
 // 오늘 하루 안 보기 만료 시간 storage 저장
 
 function setStorage(name, exp) {
-  // 만료 시간 구하기(exp를 ms단위로 변경)
+  // 만료 시간 구하기(exp를 ms단위로 변경) // 시간 수정 필요
   let date = new Date();
-  date = date.setTime(date.getTime() + exp * 60 * 1000);
+  date = date.setTime(date.getTime() + exp * 60 * 60 * 24 * 1000);
 
   // 로컬 스토리지에 저장하기
   localStorage.setItem(name, date);
 }
 
-hideButton.addEventListener("click", hidePopup);
-closeButton.addEventListener("click", closePopup);
+// hideButton.addEventListener("click", hidePopup);
+// closeButton.addEventListener("click", closePopup);
+
+buttonGroup.addEventListener("click", (e) => {
+  const target = e.target;
+  const result = target.closest("button");
+  const name = result.getAttribute("id");
+  if (!name) return;
+
+  if (name == "hidePopup") {
+    hidePopup();
+  } else {
+    closePopup();
+  }
+});
