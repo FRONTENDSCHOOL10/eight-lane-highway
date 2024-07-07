6c9a196c793363d47d4890e8f6b51939c2a1d204
채소 <chlthgus0901@naver.com>
2024-07-07 18:29:36 +0900

    docs: lib삭제
    - lib삭제

--- src/lib/index.js
+++
@@ -1,9 +0,0 @@
-// 최종 entries
-
-// re-export
-
-export * from './dom/index.js';
-export * from './utils/index.js';
-export * from './math/index.js';
-export * from './error/index.js';
-export * from './animation/index.js';
--- src/lib/math/index.js
+++
@@ -1,6 +0,0 @@
-
-
-
-export * from './toDeg.js'
-export * from './toRad.js'
-export * from './getRandom.js'
