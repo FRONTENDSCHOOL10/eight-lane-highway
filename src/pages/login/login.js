f0cb034017232ebce3df7dc3c3ce1a0e4aed1a29
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-07-08 17:34:34 +0900

    bug: header.scss
    헤더 마진 충돌 해결

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,6 +4,12 @@ const pb = new PocketBase("https://eight-lane-highway.pockethost.io/");
 
 const loginButton = document.querySelector(".login-form__button");
 
+async function getLocalItems(key) {
+  if (typeof key === "string") {
+    return await JSON.parse(localStorage.getItem(key));
+  }
+}
+
 function handleLogin(e) {
   e.preventDefault(e);
 
@@ -14,12 +20,19 @@ function handleLogin(e) {
     .authWithPassword(userId, userPw)
     .then(
       async () => {
-        const { record, token } = await pb
-          .collection("users")
-          .authWithPassword(userId, userPw);
+        const { model, token } = await JSON.parse(
+          localStorage.getItem("pocketbase_auth")
+        );
+
+        console.log(model, token);
+
+        localStorage.setItem(
+          "auth",
+          JSON.stringify({ model: model, token: token })
+        );
 
         alert("🎉로그인에 성공하셨습니다🎉");
-        location.href = "/";
+        // location.href = "/";
       },
       () => {
         alert("잘못된 로그인 또는 비밀번호 정보입니다!");
