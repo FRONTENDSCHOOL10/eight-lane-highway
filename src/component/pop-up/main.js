45d5cfbee9b59bb4623751646f543bfb6951df94
채소 <chlthgus0901@naver.com>
2024-07-08 14:25:16 +0900

    feat: 마크업 수정
    - dialog에서 div로 수정

--- src/component/pop-up/main.js
+++ src/component/pop-up/main.js
@@ -1,4 +1,5 @@
 const popup = document.querySelector(".pop-up-container");
+import { addClass } from "../../lib/dom/css";
 const hideButton = document.querySelector("#hidePopup");
 const closeButton = document.querySelector("#closePopup");
 const buttonGroup = document.querySelector(".button-container");
@@ -6,8 +7,7 @@ const buttonGroup = document.querySelector(".button-container");
 // 팝업창 닫기
 // 모달창 close함수로 닫고 display none으로 보이지 않게 처리
 function closePopup() {
-  popup.close();
-  popup.style.display = "none";
+  addClass();
 }
 
 // 현재시간과 로컬스토리지에 저장된 시간값 비교 => 현재시간이 더 크다면 팝업창 열지않기
@@ -16,10 +16,11 @@ async function isPopupLoading() {
   now = now.setTime(now.getTime());
   let hideToday = (await localStorage.getItem("popupExp")) > now;
   if (hideToday) {
-    popup.close();
+    // popup.close();
     popup.style.display = "none";
   }
-  popup.showModal();
+
+  // popup.showModal();
 }
 
 //페이지 로딩시 팝업창 띄울지 여부 확인
@@ -27,7 +28,7 @@ document.addEventListener("DOMContentLoaded", isPopupLoading);
 
 // 팝업 하루동안 안보이기
 function HidePopupToday() {
-  popup.close();
+  // popup.close();
   popup.style.display = "none";
   setStorage("popupExp", 1);
 }
