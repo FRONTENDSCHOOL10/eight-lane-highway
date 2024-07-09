6e5a470157410f7fc9002a7dab1742677dbd02af
채소 <chlthgus0901@naver.com>
2024-07-09 14:00:48 +0900

    feat : 데이터 베이스 연결
    - 데이터베이스로 상품정보 연결

--- src/component/recent-product/main.js
+++ src/component/recent-product/main.js
@@ -1,18 +1,49 @@
-import { getNode } from "../../lib/dom/getNode";
-import { insertAfter } from "../../lib/dom/insert";
+import { getNode, insertAfter, setStorage, insertLast } from "../../lib/index";
+
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase("https://eight-lane-highway.pockethost.io");
+const url = "https://eight-lane-highway.pockethost.io";
 
 const upperArrow = getNode(".upper-arrow");
 const belowArrow = getNode(".below-arrow");
 const product1 = getNode("#product1");
 const itemContainer = getNode(".recent-product-item-container");
 
-function handleAddProduct() {
-  let item = document.createElement("div");
-  item.className = "addedProduct";
-  itemContainer.insertAdjacentElement("afterend", item);
-  console.log(item);
+// function handleAddProduct() {
+//   let item = document.createElement("div");
+//   item.className = "addedProduct";
+//   itemContainer.insertAdjacentElement("afterend", item);
+//   console.log(item);
+
+//   setStorage("key", "product1");
+// }
+
+// product1.addEventListener("click", handleAddProduct);
+
+function getPbImageURL(item, fileName = "photo") {
+  return `${url}/api/files/${item.collectionId}/${item.id}/${item[fileName]}`;
 }
 
-product1.addEventListener("click", handleAddProduct);
+// async function getData() {
+//   const data = await pb.collection("products").getOne("agiethsdi3a56uo");
+//   const { name, subtitle, price, seller, packaging, unit } = data;
+//   const photo = getPbImageURL(data);
+//   console.log(name, photo);
+// }
+// getData();
 
-// console.log(upperArrow, belowArrow, product1);
+async function setData() {
+  const data = await pb.collection("products").getOne("agiethsdi3a56uo");
+  const { name, subtitle, price, seller, packaging, unit, photo } = data;
+
+  const template = `<div class="addedProduct">
+    <img src="${getPbImageURL(data)}" alt="" />
+  </div>`;
+
+  insertLast(".recent-product-item-container", template);
+
+  console.log(getPbImageURL(data));
+}
+
+setData();
