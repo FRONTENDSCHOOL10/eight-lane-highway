e85a9677a77be866ea8139d35064770c7238ca73
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-07-09 17:35:47 +0900

    refactor: 경로 수정과 build를 위한 작업 - 경로를 절대경로로 수정 - async 없이 사용한 await 요소 삭제 - 새로운 페이지들 vite.config.js에 추가

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,35 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir:'docs',
+    outDir: "docs",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        
+        main: resolve(__dirname, "index.html"),
+        header: resolve(__dirname, "src/component/header/header.html"),
+
+        register: resolve(__dirname, "src/pages/signup/index.html"),
+        productinfo: resolve(
+          __dirname,
+          "src/component/product/product-info.html"
+        ),
+        product: resolve(__dirname, "src/component/product/product.html"),
+        popup: resolve(__dirname, "src/component/pop-up/index.html"),
+        recentproduct: resolve(
+          __dirname,
+          "src/component/recent-product/index.html"
+        ),
+        login: resolve(__dirname, "src/pages/login/index.html"),
+        productdetail: resolve(
+          __dirname,
+          "src/component/product/product-detail.html"
+        ),
+        addshoppingbasket: resolve(
+          __dirname,
+          "src/component/addShoppingBasket/addShoppingBasket.html"
+        ),
       },
     },
-    
   },
-})
+});
