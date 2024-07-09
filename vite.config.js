a0ba8ab33a19b70a02ade2db34997e58fecdf663
woolen-h <woolen_h@kakao.com>
2024-07-10 03:00:46 +0900

    feat: 베이스 설정

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/eight-lane-highway/",
   build: {
     outDir: "docs",
     rollupOptions: {
