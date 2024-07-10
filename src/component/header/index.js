e5bdf3c8c39fdd7b9213080ac9987da091bd0cfe
woolen-h <woolen_h@kakao.com>
2024-07-10 13:44:58 +0900

    refactor: src 파일 이동

---
+++ src/component/header/index.js
@@ -0,0 +1,8 @@
+import logo from "/public/images/logo.svg";
+import readingGlasses from "/public/images/reading-glasses.svg";
+import address from "/public/images/address.svg";
+import cart from "/public/images/cart.svg";
+import heart from "/public/images/heart.svg";
+import newSvg from "/public/images/new.svg";
+
+export { logo, readingGlasses, address, cart, heart, newSvg };
