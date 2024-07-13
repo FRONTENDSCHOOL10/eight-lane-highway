2949a5b9a7131ec173db8a8984915a24a906fc7b
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-07-13 15:08:35 +0900

    feat: +/- 버튼 클릭 시 총 가격 변경하기 - +/- 버튼 누를 때마다 총 가격이 변경되도록 함 - 0보다 값이 작으면 alert창으로 경고 후 가격을 1로 초기화

--- src/component/addShoppingBasket/addShoppingBasket.js
+++ src/component/addShoppingBasket/addShoppingBasket.js
@@ -1,6 +1,7 @@
 const counterButton = document.querySelector(".price_counter");
 const productPrice = document.getElementById("product__price");
 const countProductNum = document.getElementById("product__num");
+const totalProduct = document.getElementById("total");
 
 function handleCounterProduct(e) {
   e.preventDefault();
@@ -8,11 +9,21 @@ function handleCounterProduct(e) {
   const target = e.target.closest("button");
 
   if (!target) return;
-
+  
   if (target.id === "plusBtn") {
     countProductNum.textContent = parseInt(countProductNum.textContent) + 1;
-  } else if (target.id === "minusBtn") {
+    totalProduct.textContent = parseInt(totalProduct.textContent) + parseInt(productPrice.textContent) + "원";
+  } 
+  else if (target.id === "minusBtn") {
+    if(parseInt(countProductNum.textContent) <= 0 )
+      {
+        alert('상품 개수가 0보다 작으면 안됩니다!');
+        countProductNum.textContent = 1;
+        totalProduct.textContent = 4980 + '원';
+        return;
+      }
     countProductNum.textContent = parseInt(countProductNum.textContent) - 1;
+    totalProduct.textContent = parseInt(totalProduct.textContent) - parseInt(productPrice.textContent) + '원';
   }
 }
 
