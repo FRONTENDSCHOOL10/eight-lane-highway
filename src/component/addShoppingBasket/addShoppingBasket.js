2cf86a249bfa91dcb23dcd9c0e26b99e466ead4f
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-07-13 16:13:43 +0900

    feat: 팝업 관련 마크업, 스타일링 수정 및 팝업 기능 추가 - 팝업 구현을 위한 마크업 수정 - 팝업을 위해 필요한 스타일링 수정 - 팝업을 버튼을 클릭했을 시 팝업이 올라오고 취소 버튼 클릭 시 팝업이 사라지도록 변경하는 함수 추가

--- src/component/addShoppingBasket/addShoppingBasket.js
+++ src/component/addShoppingBasket/addShoppingBasket.js
@@ -3,13 +3,18 @@ const productPrice = document.getElementById("product__price");
 const countProductNum = document.getElementById("product__num");
 const totalProduct = document.getElementById("total");
 
+const popup = document.querySelector('.modal');
+const cancelButton = document.querySelector('.cancel');
+const button = document.getElementById('button');
+
+
 function handleCounterProduct(e) {
   e.preventDefault();
 
   const target = e.target.closest("button");
 
   if (!target) return;
-  
+
   if (target.id === "plusBtn") {
     countProductNum.textContent = parseInt(countProductNum.textContent) + 1;
     totalProduct.textContent = parseInt(totalProduct.textContent) + parseInt(productPrice.textContent) + "원";
@@ -27,4 +32,21 @@ function handleCounterProduct(e) {
   }
 }
 
+// 팝업 관련 함수
+function openCartPopUp(e){
+  e.preventDefault();
+
+  popup.style.display = 'flex';
+  document.body.style.overflow = 'hidden'; // 팝업 모달 여는 동안 스크롤 못하게 하도록 함
+}
+
+function closeCartPopUp(e){
+  e.preventDefault();
+
+  popup.style.display = 'none';
+  document.body.style.overflow = 'visible';
+}
+
 counterButton.addEventListener("click", handleCounterProduct);
+button.addEventListener('click', openCartPopUp);
+cancelButton.addEventListener('click', closeCartPopUp);
