024b580ef22b8bd3c02e579bdf151e4993ca67f7
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-07-16 09:16:47 +0900

    feat: check 박스 클릭 시 확인하는 함수 만들기 - 각각의 어떤 요소를 클릭 했는지 확인할 수 있도록 초기 작성

--- src/component/accodian/accodian.js
+++ src/component/accodian/accodian.js
@@ -1,9 +1,14 @@
 import { toggleClass } from "../../lib";
 
 const titles = document.querySelectorAll(".accodian__menu__main__items__title");
+const itemLists = document.querySelectorAll(".accodian__sub__items__info");
 
 titles.forEach((item) => {
   item.addEventListener("click", toggleTitle);
+});
+
+itemLists.forEach((item) => {
+  item.addEventListener("click", handleClickedItems);
 });
 
 function toggleTitle(e) {
@@ -17,4 +22,18 @@ function toggleTitle(e) {
 
   toggleClass(subMenu, "is__clicked");
   svgElement.classList.toggle("rotate");
+}
+
+function handleClickedItems(e) {
+  const target = e.target.closest("input");
+
+  if (!target) return;
+
+  const isChecked = target.checked;
+  console.log(isChecked);
+
+  const labelElement = target.nextElementSibling;
+
+  console.log(target);
+  console.log(labelElement.textContent);
 }
