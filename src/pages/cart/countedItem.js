907aa2c05610a76a283a3728018021c28d979a15
채소 <chlthgus0901@naver.com>
2024-07-16 22:45:51 +0900

    feat : 선택수량 및 전체수량 반영 동기화
    - 위아래 선택수량 및 전체수량 반영 동기화

--- src/pages/cart/countedItem.js
+++ src/pages/cart/countedItem.js
@@ -3,11 +3,13 @@ export function updateSelectedCount() {
   const checkboxes = Array.from(
     document.querySelectorAll(".food-type__accordion__input")
   );
-  const checked = document.querySelector(".checkbox__check-all__label");
+  const checked = document.querySelectorAll(".checkbox__check-all__label");
   const selectedCount = checkboxes.filter(
     (checkbox) => checkbox.checked
   ).length;
-  checked.textContent = `전체선택 (${selectedCount}/${checkboxes.length})`;
+  Array.from(checked).forEach((item) => {
+    item.textContent = `전체선택 (${selectedCount}/${checkboxes.length})`;
+  });
 }
 
 // 전체 상품 갯수 랜더링 및 상품 선택시 선택된 상품 갯수 업데이트
