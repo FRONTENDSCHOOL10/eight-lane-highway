d1fb6dd740f40a610cb5d4a3ea7308d82bbb3791
woolen-h <woolen_h@kakao.com>
2024-07-17 13:45:47 +0900

    Refactor: 버블 스크립트 파일 이름 및 경로 변경

--- src/pages/product/addToCartBubble.js
+++ src/pages/product/addToCartBubble.js
@@ -1,25 +1,15 @@
-import { getPbImageURL } from "/src/api/getPbImageURL";
-
-function displayProductDataInBubble(data) {
-  const productBubble = document.querySelector(".product__bubble");
-  const productImg = productBubble.querySelector("#productImg");
-  const productName = productBubble.querySelector("#productName");
-
-  productImg.src = getPbImageURL(data);
-  productName.textContent = data.name;
-}
-
-displayProductDataInBubble();
-
-class AddToCartBubble extends HTMLElement {
+export class AddToCartBubble extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: "open" });
 
     // Create the bubble container
     const bubble = document.createElement("div");
-    bubble.className = "product__bubble";
+    bubble.className = "bubble";
     bubble.innerHTML = `
+    <link rel="stylesheet" href="/src/styles/style.scss" />
+    
+    <div class="product__bubble">
       <div class="bubble__image__container">
         <img id="productImg" />
       </div>
@@ -27,8 +17,9 @@ class AddToCartBubble extends HTMLElement {
         <h3 id="productName">상품이름</h3>
         <p>장바구니에 상품을 담았습니다.</p>
       </div>
+      </div>
     `;
-
+    bubble.style.display = "none";
     shadow.appendChild(bubble);
 
     // Store a reference to the bubble element
@@ -52,10 +43,3 @@ class AddToCartBubble extends HTMLElement {
 }
 
 customElements.define("add-to-cart-bubble", AddToCartBubble);
-
-document
-  .getElementById("addToCartButton")
-  .addEventListener("click", function () {
-    const bubble = document.querySelector("add-to-cart-bubble");
-    bubble.show("path_to_product_image", "상품 이름"); // Replace with actual product data
-  });
