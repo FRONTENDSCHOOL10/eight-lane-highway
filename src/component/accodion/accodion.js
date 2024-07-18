88ea34d69cbd2dfbf42f3d6462ec24816dfdb6b6
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-07-18 11:46:31 +0900

    feat: 필터 함수(미완성) - 필터 함수를 위한 함수 제작(미완) - input과 라벨 연결을 위한 id값 부여

--- src/component/accodion/accodion.js
+++ src/component/accodion/accodion.js
@@ -24,7 +24,7 @@ export function toggleTitle(e) {
   svgElement.classList.toggle("rotate");
 }
 
-const selectedFilters = {
+export const selectedFilters = {
   categories: [],
   brands: [],
   prices: [],
@@ -54,114 +54,114 @@ export function handleClickedItems(e) {
   const isChecked = target.checked;
   const labelElement = target.nextElementSibling.textContent;
 
-  if (target.checked) {
-    addToFilters(labelElement, target);
-  } else {
-    removeFilter(labelElement);
-  }
+  // if (target.checked) {
+  //   addToFilters(labelElement, target);
+  // } else {
+  //   removeFilter(labelElement);
+  // }
 }
 
-function addToFilters(label, target) {
-  if (target.closest("ul").parentElement.textContent.includes("카테고리")) {
-    if (!selectedFilters.categories.includes(label)) {
-      selectedFilters.categories.push(label);
-    }
-  } else if (
-    target.closest("ul").parentElement.textContent.includes("브랜드")
-  ) {
-    if (!selectedFilters.brands.includes(label)) {
-      selectedFilters.brands.push(label);
-    }
-  } else if (target.closest("ul").parentElement.textContent.includes("가격")) {
-    if (!selectedFilters.prices.includes(label)) {
-      selectedFilters.prices.push(label);
-    }
-  } else if (target.closest("ul").parentElement.textContent.includes("배송")) {
-    if (!selectedFilters.delivery.includes(label)) {
-      selectedFilters.delivery.push(label);
-    }
-  } else if (target.closest("ul").parentElement.textContent.includes("유형")) {
-    if (!selectedFilters.types.includes(label)) {
-      selectedFilters.types.push(label);
-    }
-  } else if (target.closest("ul").parentElement.textContent.includes("혜택")) {
-    if (!selectedFilters.benefits.includes(label)) {
-      selectedFilters.benefits.push(label);
-    }
-  } else if (
-    target.closest("ul").parentElement.textContent.includes("특정상품 제외")
-  ) {
-    if (!selectedFilters.exceptions.includes(label)) {
-      selectedFilters.exceptions.push(label);
-    }
-  }
-  console.log(selectedFilters);
-}
+// function addToFilters(label, target) {
+//   if (target.closest("ul").parentElement.textContent.includes("카테고리")) {
+//     if (!selectedFilters.categories.includes(label)) {
+//       selectedFilters.categories.push(label);
+//     }
+//   } else if (
+//     target.closest("ul").parentElement.textContent.includes("브랜드")
+//   ) {
+//     if (!selectedFilters.brands.includes(label)) {
+//       selectedFilters.brands.push(label);
+//     }
+//   } else if (target.closest("ul").parentElement.textContent.includes("가격")) {
+//     if (!selectedFilters.prices.includes(label)) {
+//       selectedFilters.prices.push(label);
+//     }
+//   } else if (target.closest("ul").parentElement.textContent.includes("배송")) {
+//     if (!selectedFilters.delivery.includes(label)) {
+//       selectedFilters.delivery.push(label);
+//     }
+//   } else if (target.closest("ul").parentElement.textContent.includes("유형")) {
+//     if (!selectedFilters.types.includes(label)) {
+//       selectedFilters.types.push(label);
+//     }
+//   } else if (target.closest("ul").parentElement.textContent.includes("혜택")) {
+//     if (!selectedFilters.benefits.includes(label)) {
+//       selectedFilters.benefits.push(label);
+//     }
+//   } else if (
+//     target.closest("ul").parentElement.textContent.includes("특정상품 제외")
+//   ) {
+//     if (!selectedFilters.exceptions.includes(label)) {
+//       selectedFilters.exceptions.push(label);
+//     }
+//   }
+//   console.log(selectedFilters);
+// }
 
-function removeFilter(label) {
-  Object.keys(selectedFilters).forEach((key) => {
-    selectedFilters[key] = selectedFilters[key].filter(
-      (item) => item !== label
-    );
-  });
+// function removeFilter(label) {
+//   Object.keys(selectedFilters).forEach((key) => {
+//     selectedFilters[key] = selectedFilters[key].filter(
+//       (item) => item !== label
+//     );
+//   });
 
-  console.log(selectedFilters);
-}
+//   console.log(selectedFilters);
+// }
 
-function checkPriceFilter(price) {
-  if (selectedFilters.prices.includes("6,900원 미만")) {
-    if (price < 6900) return true;
-  }
-  if (selectedFilters.prices.includes("6,900원 ~ 9,900원")) {
-    if (price >= 6900 && price <= 9900) return true;
-  }
-  if (selectedFilters.prices.includes("9,900원 ~ 14,500원")) {
-    if (price > 9900 && price <= 14500) return true;
-  }
-  if (selectedFilters.prices.includes("14,500원 이상")) {
-    if (price > 14500) return true;
-  }
-  return false;
-}
+// function checkPriceFilter(price) {
+//   if (selectedFilters.prices.includes("6,900원 미만")) {
+//     if (price < 6900) return true;
+//   }
+//   if (selectedFilters.prices.includes("6,900원 ~ 9,900원")) {
+//     if (price >= 6900 && price <= 9900) return true;
+//   }
+//   if (selectedFilters.prices.includes("9,900원 ~ 14,500원")) {
+//     if (price > 9900 && price <= 14500) return true;
+//   }
+//   if (selectedFilters.prices.includes("14,500원 이상")) {
+//     if (price > 14500) return true;
+//   }
+//   return false;
+// }
 
-export function filterProducts(data) {
-  const allProducts = data;
-  console.log(allProducts);
-  const filteredItems = allProducts.filter((item) => {
-    const matchesCategory =
-      selectedFilters.categories.length === 0 ||
-      selectedFilters.categories.includes(item.category);
-    const matchesBrand =
-      selectedFilters.brands.length === 0 ||
-      selectedFilters.brands.includes(item.brand);
-    const matchesPrice = checkPriceFilter(item.price);
-    const matchesDelivery =
-      selectedFilters.delivery.length === 0 ||
-      selectedFilters.delivery.includes(item.delivery);
-    const matchesType =
-      selectedFilters.types.length === 0 ||
-      selectedFilters.types.includes(item.type);
-    const matchesBenefit =
-      selectedFilters.benefits.length === 0 ||
-      selectedFilters.benefits.includes(item.benefit);
-    const matchesException =
-      selectedFilters.exceptions.length === 0 ||
-      !selectedFilters.exceptions.includes(item.exception);
+// export function filterProducts(data) {
+//   const allProducts = data;
+//   console.log(allProducts);
+//   const filteredItems = allProducts.filter((item) => {
+//     const matchesCategory =
+//       selectedFilters.categories.length === 0 ||
+//       selectedFilters.categories.includes(item.category);
+//     const matchesBrand =
+//       selectedFilters.brands.length === 0 ||
+//       selectedFilters.brands.includes(item.brand);
+//     const matchesPrice = checkPriceFilter(item.price);
+//     const matchesDelivery =
+//       selectedFilters.delivery.length === 0 ||
+//       selectedFilters.delivery.includes(item.delivery);
+//     const matchesType =
+//       selectedFilters.types.length === 0 ||
+//       selectedFilters.types.includes(item.type);
+//     const matchesBenefit =
+//       selectedFilters.benefits.length === 0 ||
+//       selectedFilters.benefits.includes(item.benefit);
+//     const matchesException =
+//       selectedFilters.exceptions.length === 0 ||
+//       !selectedFilters.exceptions.includes(item.exception);
 
-    return (
-      matchesCategory &&
-      matchesBrand &&
-      matchesPrice &&
-      matchesDelivery &&
-      matchesType &&
-      matchesBenefit &&
-      matchesException
-    );
-  });
+//     return (
+//       matchesCategory &&
+//       matchesBrand &&
+//       matchesPrice &&
+//       matchesDelivery &&
+//       matchesType &&
+//       matchesBenefit &&
+//       matchesException
+//     );
+//   });
 
-  renderFilteredProductList(filteredItems);
-}
+//   renderFilteredProductList(filteredItems);
+// }
 
-function renderFilteredProductList(filteredItems) {
-  console.log(filteredItems);
-}
+// function renderFilteredProductList(filteredItems) {
+//   console.log(filteredItems);
+// }
