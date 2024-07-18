649f19517142160fc33701f8b3820ddcf0d3c3e6
채소 <chlthgus0901@naver.com>
2024-07-18 09:34:56 +0900

    feat : 카운트 박스관련 기능 개선
    - 카운트 박스관련 기능 개선 구현중

--- src/pages/cart/updatePrice.js
+++ src/pages/cart/updatePrice.js
@@ -1,5 +1,6 @@
-import { formatPrice } from "../../lib";
+import { formatPrice, removeClass } from "../../lib";
 import { valueTrimmed } from "./sumAllPrice";
+import { addClass } from "/src/lib/index.js";
 
 // 수량 변경 및 가격변경 함수
 export function countNumber() {
@@ -13,24 +14,36 @@ export function countNumber() {
     const priceDiscount = set.querySelector(
       ".cart__accordion__price__discount"
     );
+    const minusImg = minusButton.querySelector("img");
     let count = Number(counterElement.textContent);
+    if (count <= 1) {
+      addClass(minusImg, "is__deactivate");
+    }
+
     const dataPrice = valueTrimmed(priceValue) / count;
     const dataDiscount = valueTrimmed(priceDiscount) / count;
 
     minusButton.addEventListener("click", () => {
-      if (count > 0) {
+      removeClass(minusImg, "is__deactivate");
+      if (count > 1) {
+        removeClass(minusImg, "is__deactivate");
         count--;
         counterElement.textContent = count;
         priceValue.textContent = `${formatPrice(count * dataPrice)}원`;
         priceDiscount.textContent = `${formatPrice(count * dataDiscount)}원`;
       }
+      addClass(minusImg, "is__deactivate");
     });
 
     plusButton.addEventListener("click", () => {
       count++;
+      removeClass(minusImg, "is__deactivate");
       counterElement.textContent = count;
       priceValue.textContent = `${formatPrice(count * dataPrice)}원`;
       priceDiscount.textContent = `${formatPrice(count * dataDiscount)}원`;
     });
   });
 }
+
+// const minusImg = minusButton.querySelector("img");
+//     removeClass(minusButton.querySelector("img"), "is__deactivate");
