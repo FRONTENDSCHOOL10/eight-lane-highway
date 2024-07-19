be27f9c2204a10a48940376e97ee14483f6371e6
heejae <leeheejaelcode@gmail.com>
2024-07-19 11:17:14 +0900

    refactor : 안쓰는 index.js 삭제

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,4 +2,3 @@ export * from "./CustomSwiper.js";
 export * from "./renderProductItem.js";
 export * from "./removeRecentProducts.js";
 export * from "./recentProducts.js";
-export * from "./shoppingCart.js";
